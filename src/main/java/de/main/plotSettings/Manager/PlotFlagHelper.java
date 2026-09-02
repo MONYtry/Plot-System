d3816426cb7b5e676565f0ec891e59d6ef216483
MONYtry <binderleon88@gmail.com>
2026-09-02 02:10:07 +0200

    Fixed EntityCap Display Error!

--- src/main/java/de/main/plotSettings/Manager/PlotFlagHelper.java
+++ src/main/java/de/main/plotSettings/Manager/PlotFlagHelper.java
@@ -2,13 +2,14 @@ package de.main.plotSettings.Manager;
 
 import com.plotsquared.core.plot.Plot;
 import com.plotsquared.core.plot.flag.implementations.AnimalCapFlag;
+import com.plotsquared.core.plot.flag.implementations.EntityCapFlag;
 import com.plotsquared.core.plot.flag.implementations.MobCapFlag;
 
 public class PlotFlagHelper {
 
     public static int getEntityCap(Plot plot)
     {
-        return plot.getFlag(MobCapFlag.class);
+        return plot.getFlag(EntityCapFlag.class);
     }
 
     public static int getAnimalCap(Plot plot)
